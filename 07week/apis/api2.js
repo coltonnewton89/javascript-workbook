7fced0313cd53fcb9835138424fb1ab0044cf19d
coltonnewton89 <52718393+coltonnewton89@users.noreply.github.com>
2019-10-28 16:54:41 -0500

    colapi api up and working. next step going to brewery!

--- 07week/apis/api2.js
+++ 07week/apis/api2.js
@@ -1,82 +1,43 @@
-const arrOfPeople = [
-    {
-      id: 2,
-      name: "Charles Young",
-      age: 55,
-      skillSet: "welding",
-      placeBorn: "Omaha, Nebraska"
-    },
-    {
-      id: 3,
-      name: "Judy Twilight",
-      age: 35,
-      skillSet: "fishing",
-      placeBorn: "Louisville, Kentucky"
-    },
-    {
-      id: 4,
-      name: "Cynthia Doolittle",
-      age: 20,
-      skillSet: "tic tac toe",
-      placeBorn: "Pawnee, Texas"
-    },
-    {
-      id: 5,
-      name: "John Willouby",
-      age: 28,
-      skillSet: "pipe fitting",
-      placeBorn: "New York, New York"
-    },
-    {
-      id: 6,
-      name: "Stan Honest",
-      age: 20,
-      skillSet: "boom-a-rang throwing",
-      placeBorn: "Perth, Australia"
-    },
-    {
-      id: 7,
-      name: "Mia Watu",
-      age: 17,
-      skillSet: "acrobatics",
-      placeBorn: "Los Angeles, California"
-    },
-    {
-      id: 8,
-      name: "Walter Cole",
-      age: 32,
-      skillSet: "jump rope",
-      placeBorn: "New Orleans, Louisiana"
-    },
-  ]
-  
-  const listOfPlayers = []
-  const blueTeam = []
-  const redTeam = []
-  
-  class player {
-    constructor(){}
-  }
-  class blueTeammate {
-    constructor(){}
-  }
-  class redTeammate {
-    constructor(){}
-  }
-  
-  const listPeopleChoices = () => {
-    const listElement = document.getElementById('people')
-    arrOfPeople.map(person => {
-      const li = document.createElement("li")
-      const button = document.createElement("button")
-      button.innerHTML = "Make Player"
-      button.addEventListener('click', function() {makePlayer(person.id)} )
-      li.appendChild(button)
-      li.appendChild(document.createTextNode(person.name + " - " + person.skillSet))
-      listElement.append(li)
+window.onload = function () {
+  console.log("Javascript completed");
+  populateUsers();
+};
+
+let populateUsers = function () {
+  fetch("https://randomuser.me/api/?results=10")
+    .then(function (res) {
+      console.log("processing the result of the api feth call = ", res);
+      return res.json();
     })
-  }
-  
-  const makePlayer = (id) => {
-    console.log(`li ${id} was clicked!`)
-  }
+    .then(function (payload) {
+      console.log("Processing the payload of the fetch call = ", payload);
+      console.log("List of users = ", payload.results);
+      payload.results.forEach(function (user, index) {
+        let li = document.createElement("li");
+        let img = document.createElement("img");
+        let span = document.createElement("span");
+        let ul = document.getElementById("listOfUsers");
+        img.setAttribute("src", user.picture.large);
+        span.innerHTML = `${user.name.title} ${user.name.first} ${user.name.last}`;
+        let button = document.createElement("button");
+        button.innerText = "Show my Email";
+        button.setAttribute("data-email", user.email);
+        li.appendChild(img);
+        li.appendChild(span);
+        li.appendChild(button);
+        button.addEventListener("click", function (event) {
+          var clickedButton = event.target;
+          console.log("the user that was clicked", clickedButton);
+          var email = clickedButton.getAttribute("date-email");
+          console.log("the email is", email);
+          var clickedLi = clickedButton.parentElement;
+          let emailSpan = document.createElement("span");
+          emailSpan.innerText = email;
+          clickedLi.appendChild(emailSpan);
+          clickedLi.removeChild(clickedButton);
+        });
+
+        ul.appendChild(li);
+      });
+    });
+};
