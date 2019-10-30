7d565a83bf88c3d16a1568be0fce53acf98044ac
coltonnewton89 <52718393+coltonnewton89@users.noreply.github.com>
2019-10-29 20:07:34 -0500

    i hate recursion

---
+++ 08week/example.js
@@ -0,0 +1,77 @@
+'use strict'
+
+import { get } from "https";
+
+/* let sum = function (x, y) {
+    return x + y;
+}
+
+let sum1 = function (x) {
+    let sum2 = function (y) {
+        return x + y;
+
+    }
+    return sum2;
+
+}
+let partial = sum1(10);
+console.log(partial(2));
+
+
+let multiplication = function (x) {
+    let multiply = function (y) {
+        return x * y;
+    }
+    return multiply;
+} */
+
+/* class MultiplicationTable {
+    constructor(base) {
+        this.base = base;
+    }
+    expand(y) {
+        return this.base * y;
+    }
+}
+
+let by3 = new MultiplicationTable(3);
+for (var i = 0; i < 13; i++) {
+    console.log(by3.expand(i));
+} */
+
+
+//get febursual numba
+
+/*let getf = function (i) {
+    console.log("it going down for real")
+    if (i == 0) {
+        return 1;
+    } else if (i == 1) {
+        return i*i;
+    }
+    else {
+        return getf(i - 2) + getf(i - 1);
+
+    }
+}
+
+console.log(getf(8)); */
+
+
+
+
+//recursive print
+
+/* let recursiveprint = function () {
+    if (i == 0) {
+        console.log(i)
+    } else {
+        console.log(i)
+        recursiveprint(i - 1);
+    }
+}
+
+recursiveprint(10);
+*/
+
+/* recursively figure out if there is a number given, what is 2 to the 17th power */
